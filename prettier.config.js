a3b9d8c916d3c059776e52176350d4b24e02d9f4
tuzi555 <jakub.tuzar@gmail.com>
2024-08-30 09:55:58 +0200

    chore: customize prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+    printWidth: 80,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
