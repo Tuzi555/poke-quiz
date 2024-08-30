cf84ced93478ca8a3fe1a67e97678479f6f5b135
tuzi555 <jakub.tuzar@gmail.com>
2024-08-30 10:08:30 +0200

    chore: add react query and more prettier tweaks

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
     printWidth: 80,
-    trailingComma: 'es5',
+    trailingComma: "es5",
     tabWidth: 4,
     useTabs: false,
     semi: true,
-    singleQuote: true,
-    plugins: ['prettier-plugin-tailwindcss'],
+    plugins: ["prettier-plugin-tailwindcss"],
 };
 
 export default config;
